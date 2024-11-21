96ab0663c62fda36c3eee1406636f88a9e51bed8
otarta <157425197+otarta@users.noreply.github.com>
2024-11-21 13:57:48 -0500

    3 scalers

--- gui.py
+++ gui.py
@@ -4,7 +4,8 @@ class Main():
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("XJEP Control Panel")
-        self. scaleFrame = tk.Frame(self) 
+        self.root.geometry("300x300")
+        self. scaleFrame = tk.Frame() 
         self.scaleFrame.grid(row =1, column = 3, sticky = 'N', columnspan = 4)
         
     
@@ -13,12 +14,21 @@ class Main():
                                       from_ = 100,
                                       to = 0,
                                       orient = "vertical")
-        self.throttleScale.pack(padx=10, pady=10)
+        self.throttleScale.grid(row=1,column=1)
 
 
+        self.throatApertureScale = tk.Scale(self.root,
+                                      from_ = 100,
+                                      to = 0,
+                                      orient = "vertical")
+        self.throatApertureScale.grid(row=1,column=2)
 
 
-
+        self.exitApertureScale = tk.Scale(self.root,
+                                      from_ = 100,
+                                      to = 0,
+                                      orient = "vertical")
+        self.exitApertureScale.grid(row=1,column=3)
 
 
 
